a5c94f090986f14ab481ca96087ef6e54027b786
Luke Padmore <lukecpadmore@gmail.com>
2026-01-20 23:51:58 +0000

    EOD

--- optuna_hpt_lr.py
+++ optuna_hpt_lr.py
@@ -3,12 +3,17 @@ import optuna
 from utils.train import train_loop_uncond
 from models.unet import UNet
 from utils.create_dataloaders import create_mnist_train_val_loaders
+from utils.logger_utils import trial_logger
 from models.config import UNetConfig, OptimConfig
 
 train_loader, val_loader = create_mnist_train_val_loaders()
 
 
-def make_objective(train_loader, device="cuda"):
+def make_objective(
+    train_loader, val_loader=None, experiment_name="optuna_hpt", device="cuda"
+):
+    mlflow.set_experiment("hpt_lr_only/unet")
+
     def objective(trial: optuna.Trial) -> float:
         unet_cfg = UNetConfig.sample(
             trial,
@@ -30,7 +35,11 @@ def make_objective(train_loader, device="cuda"):
         optim = optim_cfg.make_optimizer(model.params())
         mlflow.set_experiment("hpt_lr_only/unet")
         # with mlflow.start_run(run_name=f"trial_{trial.number:04d}"):
-        with mlflow.start_run(run_name="smoke_test"):
+        with (
+            mlflow.start_run(run_name="smoke_test"),
+            trial_logger(f"trial_{trial.number}") as logger,
+        ):
+            # TODO: add "logger" logging and test
             mlflow.set_tag("optuna.trial_number", trial.number)
             mlflow.set_tag("hpt_stage", "lr_only")
             mlflow.log_params(unet_cfg.to_mlflow_params())
@@ -49,7 +58,7 @@ def make_objective(train_loader, device="cuda"):
             best = train_loop_uncond(
                 model=model,
                 train_loader=train_loader,
-                val_loader=None,
+                val_loader=val_loader,
                 num_epochs=3,
                 optim=optim,
                 device=device,
